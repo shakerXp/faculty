e1f9476e43233a7d72d614742836f9ce2e7cbd1c
shakerXp <33755808+shakerXp@users.noreply.github.com>
2017-11-29 15:57:49 +0200

    Add files via upload

--- p6.asm
+++ p6.asm
@@ -4,76 +4,70 @@ INCLUDE io.h
 .STACK 4096
 
 .DATA
-     ar DWORD 1 , 2 , 3 , 4 
-     y BYTE "grede 1? " , 0 
-	 x BYTE "weight 1?" , 0
-	 y1 BYTE "grede 2?" , 0 
-	 x1 BYTE "weight 2?" , 0
-	 y2 BYTE "grede 3?" , 0 
-	 x2 BYTE "weight 3?" , 0
-	 y3 BYTE "grede 4?" , 0 
-	 x3 BYTE "weight 4?" , 0
-	 y4 BYTE "Weighted Sum :" , 0 
-	 x4 BYTE "sum of weights:" , 0
-     x5 BYTE "weighted average :" , 0
+     number1 DWORD ?
+	 number2 DWORD ?
+     G1 BYTE "grade 1? " , 0 
+	 W1 BYTE "weight 1?" , 0
+	 G2 BYTE "grade 2?" , 0 
+	 W2 BYTE "weight 2?" , 0
+	 G3 BYTE "grade 3?" , 0 
+	 W3 BYTE "weight 3?" , 0
+	 G4 BYTE "grade 4?" , 0 
+	 W4 BYTE "weight 4?" , 0
+	 wsum BYTE "Weighted Sum :" , 0 
+	 sumw BYTE "sum of weights:" , 0
+     wave BYTE "weighted average :" , 0
 
-	 e1 DWORD 11 , 0
-	 e2 DWORD 11 , 0
-	res DWORD 11 DUP(?)
+	 string1 DWORD 20 , 0
+	 string2 DWORD 20 , 0
+	res DWORD 20 DUP(?)
+	res2 DWORD 20 DUP(?)
 
 .CODE
 	_MainProc PROC
+	input G1,string1,20
+	atod string1 
+	MOV ebx,eax
+	input W1,string2,20
+	atod string2 
+	MOV number2,eax
+	MUL ebx
+	MOV number1,eax
 
-	   input y , e1 , 11
-	   atod e1
-	   mov ebx , eax
-	   input x , e2 , 11
-	   atod e2
-	   mov ecx , eax
-	   imul eax ,ebx
-       mov edx , eax
+	input G2,string1,20
+	atod string1 
+	MOV ebx,eax
+	input W2,string2,20
+	atod string2 
+	ADD number2,eax
+	MUL ebx
+	ADD number1,eax
 
-       input y1 , e1 , 11
-	   atod e1
-	   mov ebx , eax
-	   input x1 , e2 , 11
-	   atod e2
-	   add ecx , eax
-	   imul ebx , eax
-       add edx ,ebx
-	   
-	   input y2 , e1 , 11
-	   atod e1
-	   mov ebx , eax
-	   input x2 , e2 , 11
-	   atod e2
-	   add ecx , eax
-	   imul ebx , eax
-       add edx ,ebx
+	input G3,string1,20
+	atod string1 
+	MOV ebx,eax
+	input W3,string2,20
+	atod string2 
+	ADD number2,eax
+	MUL ebx
+	ADD number1,eax
 
-	   input y3 , e1 , 11
-	   atod e1
-	   mov ebx , eax
-	   input x3 , e2 , 11
-	   atod e2
-	   add ecx , eax
-	   imul ebx , eax
-       add edx , ebx
+	input G4,string1,20
+	atod string1 
+	MOV ebx,eax
+	input W4,string2,20
+	atod string2 
+	ADD number2,eax
+	MUL ebx
+	ADD number1,eax
 
-	   mov ebx , edx
-	   mov eax,edx
-	    mov edx , 0
-		idiv ecx
+	dtoa res2,number2
+	output sumw,res2
 
-	   dtoa res , ebx
-	   output y4 , res
+	dtoa res,number1
+	output wsum,res
 
-	  dtoa res , ecx
-	   output x4 , res
-
-	   dtoa res , eax
-	   output x5 , res
-
+	
 		mov eax , 0
 		ret
 	_MainProc ENDP
